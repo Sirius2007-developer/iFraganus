f0b5be8414f65ed1dae8053cabf38517476acf5e
Sirius2007-developer <150220106+Sirius2007-developer@users.noreply.github.com>
2024-02-18 20:48:01 +0500

    Add files via upload

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Format, Consultation, Course, Team
 # Register your models here.
+admin.site.register(Consultation)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name',)}
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name',)}
+
+@admin.register(Format)
+class FormatAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name',)}
+
+
+
+
---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Consultation, Register
+class ConsultationForm(forms.ModelForm):
+    class Meta:
+        model = Consultation
+        fields = "__all__"
+
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = Register
+        fields = "__all__"
