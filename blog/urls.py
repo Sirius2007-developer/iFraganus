facf587cc671913d1d0e20b439673de4fc63f116
Sirius2007-developer <asadbektolqinjonov1306@gmail.com>
2024-02-20 17:39:22 +0500

    template updated

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from .views import index, formatDeleteView, formatCreateView, formatUpdateView, 
 
 urlpatterns = [
     path('', index, name="index"),
-    path('team/<slug:team>/', formatdetailview, name="formatdetail"),
-    path('team/edit/<slug>/', formatUpdateView.as_view(), name="formatUpdateView"),
+    path('format/<slug:team>/', formatdetailview, name="formatdetail"),
+    path('format/edit/<slug>/', formatUpdateView.as_view(), name="formatUpdateView"),
     path('format/delete/<slug>/', formatDeleteView.as_view(), name="formatDeleteView"),
     path('format/create', formatCreateView.as_view(), name="formatCreateView"),
+
 ]
